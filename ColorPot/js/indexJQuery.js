648e754da0df8bc65f3355c03eb1fffeb073ed46
Gary <gc1753@nyu.edu>
2017-02-17 22:42:10 -0500

    harmony fix

--- ColorPot/js/indexJQuery.js
+++ ColorPot/js/indexJQuery.js
@@ -2,7 +2,7 @@ var colorView = false;
 var pickerView = false;
 var adjView = false;
 var palView = false;
-var harView = 0;
+var harView = false;
 
 function updateAllViews() {
 	updateColorView();
@@ -47,7 +47,7 @@ function updateAdjView() {
 	}
 };
 function updatePalView() {
-	$("#pal").removeClass("transform-panel-active");
+	//$("#pal").removeClass("transform-panel-active");
 	if (palView) {
 		$("#palMI").html("expand_less");
 		$("#pal").addClass("transform-panel-expand");
@@ -59,7 +59,12 @@ function updatePalView() {
 };
 
 function updateHarView() {
-	return;
+	if (harView) {
+		$("#harMI").html("lock_open");
+	}
+	else {
+		$("#harMI").html("lock_outline");
+	}
 }
 
 function optViewReset() {
@@ -96,8 +101,8 @@ $(document).ready(function () {
 		pickerView = !pickerView;
 		updatePickerView();
 	});
-	$("#harView").click(function () {
-		harView += 1;
+	$("#harMI").click(function () {
+		harView = !harView;
 		updateHarView();
 	});
 	$(".rotate").click(function () {
@@ -128,119 +133,3 @@ $(document).ready(function () {
 		}
 	});
 });
-
-/*
-var colorPicker = (function() {
-	var config = {
-		lightModifier: 20,
-		darkModifier: 0,
-		transitionDuration: 200,
-		transitionDelay: 25,
-		variationTotal: 10
-	};
-	var state = {
-		activeColor: [0, 0, 0]
-	};
-	function init() {
-		createColorPicker(function() {
-			appendBaseColors();
-		});
-		addEventListeners();
-		setFirstColorActive(function() {
-			setFirstModifiedColorActive();
-		});
-	}
-	function setActiveBaseColor(el) {
-		$('.color.active').removeClass('active');
-		el.addClass('active');
-	}
-	function setActiveColor(el) {
-		$('.color-var.active').removeClass('active');
-		el.addClass('active');
-		state.activeColor = el.data('color').split(',');
-	}
-	function addEventListeners() {
-		$('body').on('click', '.color', function() {
-			var color = $(this).data('color').split(',');
-			setActiveBaseColor($(this));
-			hideVariations(function() {
-				createVariations(color, function() {
-					setDelays(function() {
-						showVariations();
-					});
-				});
-			});
-		});
-		$('body').on('click', '.color-var', function() {
-			setActiveColor($(this));
-			setBackgroundColor();
-		});
-	}
-	function setFirstColorActive(callback) {
-		$('.color').eq(1).trigger('click');
-		callback();
-	}
-	function setFirstModifiedColorActive() {
-		setTimeout(function() {
-			$('.color-var').eq(7).trigger('click');
-		}, 500);
-	}
-	function createColorPicker(callback) {
-		$('.color-picker').append('<div class="base-colors"></div>');
-		$('.color-picker').append('<div class="varied-colors"></div>');
-		$('.color-picker').append('<div class="active-color"></div>');
-		$('.color-picker').append('<div class="color-history"></div>');
-		callback();
-	}
-	function appendBaseColors() {
-		for (i = 0; i < config.baseColors.length; i++) {
-			$('.base-colors').append('<div class="color" data-color="' + config.baseColors[i].join() + '" style="background-color: rgb(' + config.baseColors[i].join() + ');"></div>');
-		}
-	};
-	function setBackgroundColor() {
-		$('body').css({
-			'background-color': 'rgb(' + state.activeColor + ')'
-		});
-	}
-	function createVariations(color, callback) {
-		$('.varied-colors').html('');
-		for (var i = 0; i < config.variationTotal; i++) {
-			var newColor = [];
-			for (var x = 0; x < color.length; x++) {
-				var modifiedColor = (Number(color[x]) - 100) + (config.lightModifier * i);
-				if (modifiedColor <= 0) {
-					modifiedColor = 0;
-				} else if (modifiedColor >= 255) {
-					modifiedColor = 255;
-				}
-				newColor.push(modifiedColor);
-			}
-			$('.varied-colors').append('<div data-color="' + newColor + '" class="color-var" style="background-color: rgb(' + newColor + ');"></div>');
-		}
-		callback();
-	}
-	function setDelays(callback) {
-		$('.color-var').each(function(x) {
-			$(this).css({
-				'transition': 'transform ' + (config.transitionDuration / 1000) + 's ' + ((config.transitionDelay / 1000) * x) + 's'
-			});
-		});
-		callback();
-	}
-	function showVariations() {
-		setTimeout(function() {
-			$('.color-var').addClass('visible');
-		}, (config.transitionDelay * config.variationTotal));
-	}
-	function hideVariations(callback) {
-		$('.color-var').removeClass('visible').removeClass('active');
-		setTimeout(function() {
-			callback();
-		}, (config.transitionDelay * config.variationTotal));
-	}
-	return {
-		init: init
-	};
-}());
-colorPicker.init();
-*/
